8b71f3a49630d19bb084075b45e5608c9daf2054
SoloScriptSage <vladyslavhirchukbusiness@outlook.com>
2025-10-14 23:59:25 +0200

    trying to create an .ico

--- rayband/utils/pyqt_viewer.py
+++ rayband/utils/pyqt_viewer.py
@@ -8,7 +8,7 @@ import cv2
 import numpy as np
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
 from PyQt5.QtCore import QTimer, Qt
-from PyQt5.QtGui import QImage, QPixmap, QPainter, QFont, QColor, QPen
+from PyQt5.QtGui import QImage, QPixmap, QPainter, QFont, QColor, QPen, QIcon
 import logging
 
 logger = logging.getLogger(__name__)
